7c3bb21c6ca63acecd4aae364693742724a65047
Marcin <kds@o2.pl>
2015-06-20 09:15:43 +0200

    add UserData entity add creating user data add displaying data

--- src/main/java/pl/agh/odis/PrivateController.java
+++ src/main/java/pl/agh/odis/PrivateController.java
@@ -4,6 +4,9 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import pl.agh.odis.domain.UserData;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,8 +15,16 @@ import javax.servlet.http.HttpServletResponse;
 public class PrivateController {
 
     @RequestMapping(method = RequestMethod.POST, value = "submit")
-    public String post(ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
-        return "success";
+    public ModelAndView post(ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
+        String name = request.getParameter("firstname");
+        String lastName = request.getParameter("lastname");
+        UserData userData = new UserData();
+        userData.setName(name);
+        userData.setLastname(lastName);
+        userData.persist();
+        ModelAndView modelAndView = new ModelAndView("success");
+        modelAndView.addObject("users", UserData.findAllUserDatas());
+        return modelAndView;
     }
 
 
---
+++ src/main/java/pl/agh/odis/domain/UserData.java
@@ -0,0 +1,24 @@
+package pl.agh.odis.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class UserData {
+
+    /**
+     */
+    @NotNull
+    @Size(min = 2)
+    private String name;
+
+    /**
+     */
+    @NotNull
+    @Size(min = 2)
+    private String lastname;
+}
